f29841a34d6aedf630da31c9a4a7612b52fc5d24
prateek <prateekjoshi2013@gmail.com>
2025-02-01 00:58:08 -0700

    fix

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! This is an updated Node.js app!');
+    res.send('Hello, World! This is an updated Node.js app! ');
 });
 
 app.listen(PORT, () => {
